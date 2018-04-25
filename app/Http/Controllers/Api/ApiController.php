a3f47df3d98749be52f4217f57d1acd4edec5b74
maxrisk <469057099@qq.com>
2018-04-25 16:08:30 +0800

    优化上传图片接口

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -29,10 +29,10 @@ class ApiController extends Controller
 
         // 1. code2session
         $res = $this->code2session($code);
-        $res = [
-            'session_key' => "2JAuuT1DLPtOMTKsQQbHzw==",
-            "openid" => "oA1g95ZvRJ6qSpsXVDfEYTUej1f0"
-        ];
+//        $res = [
+//            'session_key' => "2JAuuT1DLPtOMTKsQQbHzw==",
+//            "openid" => "oA1g95ZvRJ6qSpsXVDfEYTUej1f0"
+//        ];
         if (isset($res['errcode'])) {
             return json_encode($res);
         }
